9158c60da0b8fd1e808cf152570e3da3e768519c
marmstr93ng <marmstr93ng@gmail.com>
2019-04-13 19:44:17 +0100

    Removed realtime console output

--- code.py
+++ code.py
@@ -1,5 +1,3 @@
-import os
-import sys
 import logging
 import logging.config
 
@@ -11,9 +9,5 @@ def main():
     logging.info("Beginning Script")
 
 if __name__ == '__main__':
-
-    # Included to allow realtime output to the console
-    # Re-opens stdout file descriptor with write mode and 0 as the buffer size
-    sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
 
     main()
